6439e2148b8fd40ddb58d1f0f3396575a87c76e8
sanchezoemmanuel-arch <sanchezoemmanuel@gmail.com>
2026-09-05 20:39:08 -0500

    Delete breakpoint-taller-react-native/breakpoint directory

--- breakpoint-taller-react-native/breakpoint/src/hooks/useAppTheme.js
+++
@@ -1,9 +0,0 @@
-import { useColorScheme } from 'react-native';
-import { palettes } from '../constants/theme';
-
-// Adaptación claro/oscuro con useColorScheme de React Native.
-export function useAppTheme() {
-  const scheme = useColorScheme();
-  const isDark = scheme === 'dark';
-  return { scheme: scheme ?? 'light', isDark, palette: isDark ? palettes.dark : palettes.light };
-}
